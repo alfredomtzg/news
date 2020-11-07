fac97eac5286b3a46167ee03bcf4e10ad43cbc7b
Alfredo Martinez <psialfredomtz@gmail.com>
2020-11-07 13:48:48 -0600

    [update] Global style

---
+++ src/components/Style/index.js
@@ -0,0 +1,80 @@
+import styled from 'styled-components'
+import { createGlobalStyle } from "styled-components";
+
+export const Colors = {
+  bambu: '#0c3c4a',
+  black: '#3a3a3a',
+  light: '#999999',
+  darkgray: '#636262',
+  white: '#f6f6fb',
+  shadow: '3px 4px 9px 0px rgba(0, 0, 0, 0.15)',
+};
+
+export const GlobalStyle = createGlobalStyle`
+  
+  html {
+    font-family: 'Source Sans Pro', sans-serif;
+    margin: 0;
+    padding: 0;
+    font-size: 10px;
+    background-color: ${Colors.white};
+  }
+
+  a {
+    color: inherit;
+    text-decoration:none;
+  }
+
+  body{
+    margin:0;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+  
+    margin:0;
+  }
+
+  h1 {
+    font-size: 30px;
+    font-family: 'Avenir' 'Source Sans Pro', sans-serif;
+    font-weight: 900;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+  }
+
+  h5 {
+    font-family: 'Avenir' 'Source Sans Pro', sans-serif;
+  font-size: 18px;
+  font-weight: 900;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${Colors.black}
+
+  }
+
+  h6 {
+    font-family: 'OpenSans', 'Source Sans Pro', sans-serif;
+  font-size: 14px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${Colors.black}
+  }
+
+  p {
+    font-family: OpenSans;
+  font-size: 14px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${Colors.light}
+  }
+`;
--- src/container/App.js
+++ src/container/App.js
@@ -1,7 +1,12 @@
+import React from 'react'
+import { GlobalStyle } from '../components/Style';
 import Routes from '../routes'
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
   );
 }
